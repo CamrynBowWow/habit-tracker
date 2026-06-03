be8655cdfb565e76a39d06a220f271ee0269b82f
CamrynBowWow <95950447+CamrynBowWow@users.noreply.github.com>
2026-06-03 22:54:31 +0200

    Custom Hooks Done

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,1 +1,17 @@
-export function useLocalStorage<T>(key: string, initialValue: T) {}
+import { useState } from 'react';
+
+export function useLocalStorage<T>(key: string, initialValue: T) {
+	const [storedValue, setStoredValue] = useState<T>(() => {
+		try {
+			const item = localStorage.getItem(key);
+
+			if (item == null) return initialValue;
+
+			return JSON.parse(item);
+		} catch {
+			return initialValue;
+		}
+	});
+
+	return [storedValue, setStoredValue] as const;
+}
